5795ea979735d763967f01477fb075e6ff839421
Rafael Alves <alvesrafael17@hotmail.com>
2021-06-22 11:19:57 -0300

    Tela de login

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
+
+import discordImg from '../../assets/discord.png';
+import { styles } from './styles';
+
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+};
+
+export function ButtonIcon({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={discordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
